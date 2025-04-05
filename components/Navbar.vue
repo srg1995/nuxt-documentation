2dc7af668215325b8b3373c51aec6ab8e9b965d6
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-04-05 12:13:52 +0200

    navbar router

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const username = "usuario1";
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/user">User</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink :to="`/user/${username}`">{{ username }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
