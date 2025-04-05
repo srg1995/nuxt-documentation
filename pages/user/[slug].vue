95c23ee8e8c4cacd0f1cbc3ad5fb863911168c3e
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-04-05 12:05:42 +0200

    pages

---
+++ pages/user/[slug].vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const route = useRoute();
+const slug = route.params.slug;
+</script>
+
+<template>
+  <h1>User Page {{ slug }}</h1>
+</template>
+
+<style scoped></style>
